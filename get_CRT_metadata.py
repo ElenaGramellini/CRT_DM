9f387d9206e479a74efaa6375abf86177fbddf10
Elena Gramellini <elenag@uboonegpvm01.fnal.gov>
2016-11-16 17:13:18 -0600

    read first line of eventdump

--- get_CRT_metadata.py
+++ get_CRT_metadata.py
@@ -6,29 +6,29 @@ Creation Date: 2016-11-07
 Version 0 
 -----------------------------------------------------------------------
 TO DO:
-[   ] Understand how to setups without root permission
-[   ] Run the dump command
+[ x ] Understand how to setups without root permission
+[ x ] Run the dump command
 [   ] Read the dump output
 [   ] Fill variables:
-      [   ]  run           
-      [   ]  subrun        
-      [   ]  sevt          
+      [ x ]  run           
+      [ x ]  subrun        
+      [ x ]  sevt          
       [   ]  stime         
       [   ]  etime         
-      [   ]  eevt           
+      [ x ]  eevt           
       [   ]  num_events         
       [   ]  ver      
       [   ]  file_format
       [   ]  ub_project_version 
       [   ]  gps_stime_usec     
       [   ]  gps_etime_usec     
-[   ] undestand output format
+[ x ] undestand output format
 
 functions:
-dumpEvent()
-readEventDump(string)
-createMetadata()
-main
+x dumpEvent()
+  readEventDump(string)
+  createMetadata()
+x main
 
 """   
 
@@ -49,17 +49,12 @@ import subprocess
 
 import datetime, json
 import argparse
-
+'''
 # samweb include
 import samweb_cli
 import samweb_client.utility
 import extractor_dict
-import subprocess
-
-
-
-
-
+'''
 
 def createMetadata(in_file):    
     ################  Retreive the file size ################ 
@@ -71,9 +66,10 @@ def createMetadata(in_file):
     else:
         run_type='unknown'
         
-
+    
     ##################  Define the checksum ##################
     checksum       = -1
+    '''
     metadata = {}
     try:
         metadata['crc'] = samweb_client.utility.fileEnstoreChecksum( in_file )
@@ -84,32 +80,39 @@ def createMetadata(in_file):
         subject = 'Failed to obtain the checksum of the file %s' % in_file
         text = """File: %sError message: %s""" % ( in_file, errorMessage )
         statusCode = 100
+    '''
+    run            = "Bogus" #run of first event
+    subrun         = "Bogus" #subrun of first event
+    sevt           = "Bogus"  # first event (in uboone 0)
+    eevt           = "Bogus"  # last event (in uboone 49)
 
-   ##################  Read the dump file ##################
-   ##################  WARNING, THIS IS BOGUS ##################
-   #     dump_first_evt = []
-   #     dump_last_evt = []
-   #     with open(args.file_dump) as inputdump:
-   #         for line in inputdump:
-   #             dump_first_evt.append(line.rstrip('\n'))
-   #             dump_last_evt.append(line.rstrip('\n'))
-   ##########################################################
-        
-
-
-    eventdump(in_file,0)
-
-    run            = "Wait for Wes" #run of first event
-    subrun         = "Wait for Wes" #subrun of first event
-    sevt           = -1  # first event (in uboone 0)
     stime          = -1  # first event time stamp --> check the format, up to seconds
     etime          = -1  # last event time stamp --> check the format, up to seconds
-    eevt           = -1  # last event (in uboone 49)
     num_events     = -1  # CRT events are not sequential... I need to come up with a way...
     ver                = -1 # daq version
     gps_stime_usec     = -1
     gps_etime_usec     = -1 
     file_format = "not sure, need to talk to Wes"
+  
+
+    ##################  Read the dump file for first event ##################   
+    ################ THIS NEEDS TO BE CHANGED WHEN THE DAQ FILE IS NOT BOGUS
+    first_evt_dump = eventdump(in_file,3).split('\n') 
+    for line in  first_evt_dump:
+        if "run:" in line:
+            w   = line.split()
+            run    = int(w[w.index("run:")+1])           
+            subrun = int(w[w.index("subRun:")+1])
+            sevt   = int(w[w.index("event:")+1])
+
+    ##################  Read the dump file for last event ##################   
+    ################ THIS NEEDS TO BE CHANGED WHEN THE DAQ FILE IS NOT BOGUS
+    last_evt_dump = eventdump(in_file,4).split('\n') 
+    for line in  last_evt_dump:
+        if "run:" in line:
+            w   = line.split()
+            eevt   = int(w[w.index("event:")+1])
+            
                 
 
     jsonData = {'file_name': os.path.basename(in_file), 
@@ -132,26 +135,33 @@ def createMetadata(in_file):
                 'online.end_time_usec': str(gps_etime_usec)
                 }
     
-    
+    '''
+    #Just for debug
     for i in jsonData:
         print i, jsonData[i]
     print 
     print
-
+    '''
+    # Create the json file containing the metadata
     jsonFileName = os.path.basename(in_file) + ".json"
     with open(jsonFileName, 'w') as outfile:
         json.dump(jsonData, outfile)
 
 
-
+# Dump DAQ file content for 1 event given:
+# - the DAQ file name
+# - the number of events we want to skip
 def eventdump(infile,skipEvents):
     cmd = "art -c RunTimeCoincidence.fcl -s "+infile + " -n 1 "+ "--nskip "+str(skipEvents)
-    print cmd
-    p = subprocess.Popen([cmd, ''],
-                         stdout=subprocess.PIPE,  
-                         stderr=subprocess.PIPE)
+    print cmd, "$$$$$$$$"
+    p = subprocess.Popen(cmd,shell=True,
+                         stdout=subprocess.PIPE)
+     #                    stderr=subprocess.PIPE)
     out, err = p.communicate()
+    #out, err = p.communicate()
     print out
+    return out
+
 
 
 if __name__ == '__main__':
@@ -161,11 +171,12 @@ if __name__ == '__main__':
     parser.add_argument("file_list", help="this is the list of files you want to generate metadata for")
     args = parser.parse_args()
 
-
+    # Read filelist
     in_file_v = []
     with open(args.file_list) as inputlist:
         for line in inputlist:
             in_file_v.append(line.rstrip('\n'))
             
+    # Create metadata for each file in the filelist
     for in_file in in_file_v:
         createMetadata(in_file)
--- launch_get_CRT_metadata.sh
+++ launch_get_CRT_metadata.sh
@@ -11,9 +11,14 @@ if [ ! -f "$1" ]; then
 fi
 
 echo "sourcing a bunch of usefull stuff:"
-echo "uboone code source and setup"
+echo "1) uboone code source and setup"
 source /grid/fermiapp/products/uboone/setup_uboone.sh
 setup uboonecode v05_08_00 -q e9:prof
+
+echo "2) CRTdaq source and setup" 
+source /artdaq_products/setup
+setup bernfebdaq v00_03_00 -qe10:s41:eth:prof
+
 
 echo "launching python get_CRT_metadata.py for file list $1"
 python get_CRT_metadata.py $1
